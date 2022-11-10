4046bf34880ca400d43f2a5a837a855bea091218
sebash3151 <sebash31510@gmail.com>
2022-11-09 23:19:21 -0500

    vainosio

---
+++ FinalVr/Assets/PersonalScripts/InventoryManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    [SerializeField] GameObject carta;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void cartear()
+    {
+        carta.SetActive(true);
+    }
+}
