cbc4cb0168e6d47adba51541daa9c45417dd5495
sebash3151 <sebash31510@gmail.com>
2022-11-16 16:28:47 -0500

    win condition y mapa

---
+++ FinalVr/Assets/PersonalScripts/WinCondition.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinCondition : MonoBehaviour
+{
+    private AudioSource sound;
+    [SerializeField] float time = 10f;
+    private float timer = 0f;
+    private bool win = false;
+
+    private void Update()
+    {
+        if (win)
+        {
+            timer += Time.deltaTime;
+            if (timer >= time)
+            {
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+
+    public void Winner()
+    {
+        sound.Play();
+        win = true;
+    }
+}
